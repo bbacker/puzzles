974332197cce1b56ebb292c4ea366136883dc335
Bryan Backer <bbacker@ep.com>
2019-09-23 13:06:38 -0700

    add argparse

--- perm_letters.py
+++ perm_letters.py
@@ -1,28 +1,33 @@
 #!/usr/local/bin/python3
 
+# for wordscapes app:
 # for given list of letters, show all permutations
 # that are found in a dictionary
 
+import argparse
 from itertools import permutations
 
-l = ['t', 'i', 'e', 'r', 'e', 'h']
-l = ['c', 'a', 's', 'i', 'n', 'o']
-s = 'tiereh'
-s = 'casino'
-s = 'hurdle'
-s = 'snuiqt'
+ss = 'casino'
+ss = 'lremot'
 
-debug=False
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", "--verbose", type=int, help="verbose level, default 0")
+parser.add_argument("-s", "--string", type=str, help="string of target letters to permute, e.g. tireeh")
+
+args=parser.parse_args()
+ss=args.string
+
+verbose=0
 
 words = []
 
 for l in 3, 4, 5, 6:
-    perm = permutations( list(s), l)
+    perm = permutations( list(ss), l)
     for i in perm:
         w = ''.join(i)
         words.append(w)
 
-if debug:
+if verbose > 2:
     print('permutations:')
     print(words)
 
@@ -33,4 +38,5 @@ spell = SpellChecker( distance=1 )
 known = spell.known( words )
 
 print('known words:')
+#print('\n'.join(sort(known)))
 print('\n'.join(known))
